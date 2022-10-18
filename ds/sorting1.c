d81bcd59b1f5198c2e0889d06a15368e34befea6
Seshu Matta <seshu.matta@thundersoft.com>
2022-10-18 16:50:58 +0530

    Included QuickSort

---
+++ ds/sorting1.c
@@ -0,0 +1,113 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void BubbleSort(int arr[],int size);
+void Display(int arr[],int size);
+void QuickSort(int arr[],int start,int end);
+int partition(int arr[],int start,int end);
+void swap(int *x,int *y);
+
+
+
+int main()
+{
+    int ch,size,arr[20],start,end;
+    
+        printf("Enter the no. of elements into the array:\n");
+        scanf("%d",&size);
+        printf("Enter elements to be sorted:\n");
+        for(int i=0;i<size;i++)
+        {
+            scanf("%d",&arr[i]);
+        }
+        
+    start=0;
+    end=size-1;
+    
+    while(1)
+    {
+        printf("\nSorting:\n");
+        printf("\n1.BubbleSort \n2.Display\n3.QuickSort\n");
+        printf("\n\nEnter the choice:");
+        scanf("%d",&ch);
+
+        switch(ch)
+        {
+            case 1: BubbleSort(arr,size);
+                    break;
+            case 2: Display(arr,size);
+            
+            case 3: QuickSort(arr,start,end);
+            
+            
+            
+        }
+    }
+}
+
+void BubbleSort(int arr[20],int size)
+{
+    int j,k,flag;
+
+    for(j=0;j<size-1;j++)
+    {
+        int temp,flag=0;
+        for(k=0;k<size-1-j;k++)
+        {
+            if(arr[k]>arr[k+1])
+            {
+               swap(&arr[k],&arr[k+1]);
+
+                flag=1;
+            }
+        }
+        if(flag==0)
+        break;
+    }
+}
+
+void Display(int arr[20],int size)
+{
+    for(int l=0;l<size;l++)
+    {
+        printf("%d\t",arr[l]);
+    }
+}
+
+void QuickSort(int arr[],int start,int end)
+{
+    int pIndex;
+    if(start<end)
+    {
+        pIndex=partition(arr,start,end);
+        QuickSort(arr,start,pIndex-1);
+        QuickSort(arr,pIndex+1,end);
+    }
+}
+
+int partition(int arr[],int start,int end)
+{
+    int pIndex=start;
+    int pivot=arr[end];
+    int q,temp1;
+    for(q=start;q<end;q++)
+    {
+        if(arr[q]<pivot)
+        {
+           swap(&arr[q],&arr[pIndex]);
+            
+            pIndex++;
+        }
+    }
+    swap(&arr[end],&arr[pIndex]);
+    return pIndex;
+}
+
+void swap(int *x,int *y)
+{
+    int temp2;
+    temp2= *x;
+    *x= *y;
+    *y=temp2;
+    
+}
